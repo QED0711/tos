bd24ac6c15028c30340afb1437b9137840225741
QED0711 <qdizon@gmail.com>
2023-01-04 20:24:08 -0500

    updated calc script

--- thinkscripts/snippets/myfxbook_calc.js
+++ thinkscripts/snippets/myfxbook_calc.js
@@ -1,18 +1,30 @@
 // https://www.myfxbook.com/forex-calculators/pip-calculator
 
-function findSymbol(symbol){
-    symbol = symbol.replace(/\//g, "").toUpperCase();
-    const standardPip = parseFloat(document.querySelector(`#standardLot${symbol}`).innerText.slice(1))
-    const miniPip = parseFloat(document.querySelector(`#miniLot${symbol}`).innerText.slice(1))
-    const microPip = parseFloat(document.querySelector(`#microLot${symbol}`).innerText.slice(1))
-    
-    return {
-        standardPip,
-        miniPip,
-        microPip
+function getTableData(){
+
+    const body = document.querySelector("#symbols tbody")
+    const rows = body.querySelectorAll("tr")
+
+
+    let data = {};
+
+    let symbol,
+        standardLot, 
+        miniLot,
+        microLot;
+    for (let row of rows){
+        symbol = row.id
+        data[row.id.slice(0, 3) + "/" + row.id.slice(3)] = {
+            standardLot: parseFloat(row.querySelector("#standardLot" + symbol).innerText.slice(1)),
+            miniLot: parseFloat(row.querySelector("#miniLot" + symbol).innerText.slice(1)),
+            microLot: parseFloat(row.querySelector("#microLot" + symbol).innerText.slice(1)),
+
+        }
+
     }
+    data = JSON.stringify(data)
+
+    return data
 }
 
-findSymbol("USD/JPY")
-
-const positionForm = document.createElement("form");
+getTableData()
