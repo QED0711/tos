a8939df3d923c2a94d06676a7e586cf1f62a1850
QED0711 <qdizon@gmail.com>
2023-01-31 05:15:55 -0500

    - added run_commit script for faster launching - updated workspaces

---
+++ run_commit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+xhost +;
+
+docker run --rm -ti -t \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $PWD/workspaces/:/workspaces/:z \
+    -v $PWD/reports/:/root/reports/:z \
+    -v $PWD/tos.sh:/tos.sh \
+    -v $PWD/persist_workspaces.sh:/persist_workspaces.sh \
+    tos_commit:latest
